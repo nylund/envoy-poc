fc63fff7efcdd329a47c297dc7d37404fcb11880
Niklas Nylund <niklas.nylund@appstudios.fi>
2019-02-07 16:52:09 +0200

    adding docker-compose. adding REST endpoints in both services

--- Makefile
+++
@@ -1,4 +0,0 @@
-build:
-	./mvnw package
-	cd service-a && docker build -t envoy-service-a .
-	cd service-b && docker build -t envoy-service-b .
---
+++ service-a/src/main/java/com/example/servicea/Controller.java
@@ -0,0 +1,27 @@
+package com.example.servicea;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello from service a";
+    }
+
+    @GetMapping("/call-b")
+    public String callB() {
+
+        RestTemplate template = new RestTemplate();
+
+        ResponseEntity<String> response = template.getForEntity("http://service-b:8080/hello", String.class);
+
+        return "Service A called B and got response: " + response.getBody();
+    }
+
+
+}
